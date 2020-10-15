484538d8f656cab69b2b8d46d5fe5e88b5657671
Brock Mykietowich <b.mykietowich7@gmail.com>
2020-10-15 14:43:29 -0500

    Creation of Pokemon table.

---
+++ db/migrate/20201015191312_create_pokemons.rb
@@ -0,0 +1,11 @@
+class CreatePokemons < ActiveRecord::Migration[6.0]
+  def change
+    create_table :pokemons do |t|
+      t.string :name
+      t.int :pokemon_typeID
+      t.int :regionID
+
+      t.timestamps
+    end
+  end
+end
