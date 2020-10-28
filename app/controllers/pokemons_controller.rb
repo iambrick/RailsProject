c264e303427ca5c4dddde1543844d4db88633940
Brock <b.mykietowich7@gmail.com>
2020-10-28 11:48:21 -0500

    Setting up routes for future, created pokemons index/show

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,2 +1,9 @@
 class PokemonsController < ApplicationController
+  def index
+    @pokemons = Pokemon.includes(:region).all
+  end
+
+  def show
+    @pokemon = Pokemon.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :pokemons, only: [:index, :show]
+  resources :regions, only: [:index, :show]
+  resources :types, only: [:index, :show]
+  resources :abilities, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
