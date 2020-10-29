bf9de47d7ef5286e9d99b32a532a7f31c70ad64a
Brock <b.mykietowich7@gmail.com>
2020-10-28 22:22:55 -0500

    Changed output when search returns no results.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,15 @@
 class PagesController < ApplicationController
   def search
-    if params[:search].blank?
-      redirect_to('/about', alert: "Empty search!") and return
-    else
+    # if params[:search].blank?
+    #   redirect_to('/search') and return
+    # else
       @parameter = params[:search].downcase
       @pokemonResults = Pokemon.all.where("lower(pokemonName) LIKE :search", search: "%#{@parameter}%")
       @typeResults = Type.all.where("lower(name) LIKE :search", search: "%#{@parameter}%")
       @regionResults = Region.all.where("lower(name) LIKE :search", search: "%#{@parameter}%")
       @abilityResults = Ability.all.where("lower(abilityName) LIKE :search", search: "%#{@parameter}%")
-    end
+
+      @allEmpty = @pokemonResults.blank? && @typeResults.blank? && @regionResults.blank? && @abilityResults.blank?
+    # end
   end
 end
